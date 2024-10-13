2475d1da4b67b33195769f134371e4e2ad092e83
abc-abc-ab <152158429+abc-abc-ab@users.noreply.github.com>
2024-10-13 11:30:35 +0900

    Add RunFunction

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 const craft_table = document.getElementById("craftingTable"),
 recipe = document.getElementById("recipe");
+
+recipe.childNodes.item(1).addEventListener("click", (e) => {
+    e.preventDefault();
+    run(1)
+})
+
+/**
+ * @overload
+ * @returns {void}
+ * 
+ * 
+ * @overload
+ * @param {number} num 
+ * @returns {void}
+ */
+function run(num){
+    if (num !== undefined){
+        alert("It's Message.")
+    }
+    else{
+        throw new SyntaxError("It's Error.");
+    }
+}
