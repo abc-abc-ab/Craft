3f77f7e45811f6b94fa0dc25551b654d90bc8b65
abc-abc-ab <152158429+abc-abc-ab@users.noreply.github.com>
2024-10-13 14:38:40 +0900

    Add selectFunction

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ recipe.childNodes.item(1).addEventListener("click", (e) => {
     run(1)
 })
 
+/** @typedef {boolean} bool */
 /**
  * @overload
  * @returns {void}
@@ -22,4 +23,16 @@ function run(num){
     else{
         throw new SyntaxError("It's Error.");
     }
+}
+
+/**
+ * @param {HTMLElement} elm 
+ * @param {number} num1 
+ * @param {number} num2 
+ * @param {bool} isTable 
+ */
+function select(elm, num1, num2, isTable){
+    isTable?
+    elm.children.item(1).children.item(num1).children.item(num2):
+    elm.children.item(num1).children.item(num2);
 }
