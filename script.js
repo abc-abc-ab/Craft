f0902310af091be266b049ae620aaf1c308bc942
abc-abc-ab <152158429+abc-abc-ab@users.noreply.github.com>
2024-10-13 14:48:57 +0900

    num to 1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const craft_table = document.getElementById("craftingTable"),
 recipe = document.getElementById("recipe");
 
-select(recipe, 0, 0, true).addEventListener("click", (e) => {
+select(recipe, 1, 1, true).addEventListener("click", (e) => {
     e.preventDefault();
     run(1)
 })
