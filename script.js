7fc7d794bd0ed9d7175d8536fb25a310f7bdf7ae
abc-abc-ab <152158429+abc-abc-ab@users.noreply.github.com>
2024-10-14 09:07:49 +0900

    Del if

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let all = [];/* Comment */
 
 try{
 recipe.children.item(1).childNodes.forEach((v, k, p) => {
-    if (v instanceof Node){
+    /*if (v instanceof Node)*/{
         for (let index0 = 0;index0 == v.children.length - 1;index0++){
             all.push(v.children.item(index0))
         }
